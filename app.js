3245de1779a790f55de273af24433b448fa8f019
KRISHNA MOHAN <134590019+krishnamidathana@users.noreply.github.com>
2023-07-13 17:54:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const movieName = document.getElementById("movie-name");
+const searchBtn = document.getElementById("search-button");
+const result = document.getElementById("result");
+
+let getMovie = () => {
+  let moviegot = movieName.value;
+  movieName.value = "";
+  let url = `http://www.omdbapi.com/?t=${moviegot}&apikey=${key}`;
+  if (moviegot.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+  } else {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.Response == "True") {
+          result.innerHTML = `
+          <div class="info">
+          <img src=${data.Poster} class="poster">
+
+          <div><h2>${data.Title}</h2>
+
+          <div class="rating">
+          <h4>  Rating:${data.imdbRating}</h4>
+          </div>
+
+          <div class="details">
+          <span> ${data.Rated}</span>
+          <span> ${data.Year}</span>
+          <span> ${data.Runtime}</span>
+          </div>
+
+          <div class="genre">
+          <div> ${data.Genre.split(",").join("</div><div>")}</div>
+          </div>
+         </div>
+         </div>
+          <h3>Plot:</h3>
+          <p>${data.Plot}</p>
+          <h3>Cast:</h3>
+          <p>${data.Actors}</p>
+          </div>
+          
+          `;
+        } else {
+          result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+        }
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg"> Error Occured</h3>`;
+      });
+  }
+};
+
+window.addEventListener("load", getMovie);
+searchBtn.addEventListener("click", () => {
+  getMovie();
+  console.log("clicked");
+});
